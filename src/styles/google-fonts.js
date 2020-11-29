1c52fc4892253a69bd5fe752f8323f6ac174a71c
root <root@LAPTOP-IOTILJ1N.localdomain>
2020-11-29 22:32:07 +0900

    change font and style

--- src/styles/google-fonts.js
+++ src/styles/google-fonts.js
@@ -1,12 +1,23 @@
 // Open Sans
-import "fontsource-open-sans"
+// import "fontsource-open-sans"
 
 // Noto Sans KR
-import "typeface-noto-sans-kr"
+import "typeface-noto-sans-kr/500.css"  // 기본
+import "typeface-noto-sans-kr/700.css"  // 제목
+
+// Noto Serif KR
+import 'fontsource-noto-serif-kr/500.css'
+import 'fontsource-noto-serif-kr/700.css'
+
 
 // Nanum Gothic Coding
-import "fontsource-nanum-gothic-coding"
+import "fontsource-nanum-gothic-coding/400.css"
 
 // Source Code Pro
-import "fontsource-source-code-pro"
+import 'fontsource-source-code-pro/500.css'
+import 'fontsource-source-code-pro/500-italic.css'
 
+
+// Nanum Gothic
+// import 'fontsource-nanum-gothic/400.css'
+// import 'fontsource-nanum-gothic/700.css'
