39f35a125df4e4fc1644455733ad5f5e24e77e1e
ijgit <isuu.jeong@gmail.com>
2020-12-04 17:08:15 +0900

    add comment

---
+++ src/components/Comment/comment.js
@@ -0,0 +1,49 @@
+import React, { useRef, useContext, useEffect } from 'react';
+import config from './../../../_config'
+import { useColorMode } from 'theme-ui';
+
+const Comment = () => {
+  const [colorMode] = useColorMode();
+  const src="https://utteranc.es/client.js";
+  const utterancesSelector = 'iframe.utterances-frame';
+  const LIGHT_THEME = 'github-light';
+  const DARK_THEME = 'photon-dark'; // "dark-blue" 
+  const containerRef = useRef(null);
+  useEffect(()=>{
+    
+    const themeMode = colorMode === 'dark' ? DARK_THEME : LIGHT_THEME;
+    const createUtterancesEl = () => {
+      const comment = document.createElement('script');
+      const attributes = {
+        'src': `${src}`,
+        'repo': config.githubInfo,
+        'issue-term': "pathname",
+        'theme': themeMode,
+        'crossorigin': 'anonymous',
+        'async': true,
+        "chromeWebSecurity": false,
+        "modifyObstructiveCode": false
+      };
+      Object.entries(attributes).forEach(([key, value]) => {
+        comment.setAttribute(key, value);
+      });
+      containerRef.current.appendChild(comment);
+    }
+    const postThemeMessage = () => {
+      const message = {
+        type: 'set-theme',
+        theme: themeMode,
+      };
+      utterancesEl.contentWindow.postMessage(message, src);
+    };
+    const utterancesEl = containerRef.current.querySelector(utterancesSelector);
+    utterancesEl ? postThemeMessage() : createUtterancesEl();
+  }, [colorMode]);
+  return <div ref={containerRef} />;
+}
+
+export default Comment;
+
+
+
+// reference: https://www.sungikchoi.com/blog/gatsby-dark-mode/
--- src/components/theme/theme-provider.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { ThemeProvider } from 'theme-ui'
-import theme from './../../utils/theme'
-export default props => (
-  <ThemeProvider theme={theme}>
-    {props.children}
-  </ThemeProvider>
-)
