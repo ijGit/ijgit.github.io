b6eea88238c45cc42549975604fcbcdee3da7e31
ijgit <isuu.jeong@gmail.com>
2020-12-15 01:49:46 +0900

    add postrep item in index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 
 import { Layout } from "../components/layout/layout"
-import { Link, graphql } from "gatsby"
-import {TagList} from './../components/tags/tags'
+import { graphql } from "gatsby"
 import {Head} from './../components/head/head'
+import {PostRep} from './../components/post-rep/post-rep'
 import styled from "styled-components"
+
 
 const PostListContainer = styled.div`
   display: flex;
@@ -15,46 +16,9 @@ const PostList = styled.ul`
   margin-left: 0;
   padding: 0;
 `
-const PostRep = styled.li`
+const PostItem = styled.li`
   list-style: none;
   margin-bottom: 9vh;
-`
-const PostRepHeader = styled.h3`
-  font-size: 1.2rem;
-  margin-bottom: 1vh;
-`
-const PostRepDate = styled.div`
-  display: inline-block;
-  font-size: 0.85rem;
-  opacity: 0.7;
-  padding-right: .5rem;
-`
-const PostRepTags = styled.div` 
-  display: inline-block;
-  margin-left: 1rem;
-
-  font-size: 0.85rem;
-  opacity: 0.7;
-
-  .item{
-    margin-right: .5rem;
-  }
-  .item:before{
-    content: '#';
-  }
-`
-
-const PostRepExcerpt = styled.div`
-  font-size: 0.9rem;
-  opacity: 0.8;
-
-  -webkit-line-clamp: 2;
-  line-height: 1.5rem;
-  height: 3rem;
-
-  word-wrap: break-word;
-  overflow: hidden;
-  text-overflow: ellipsis;
 `
 
 export default function IndexPage({ data }) {
@@ -71,26 +35,17 @@ export default function IndexPage({ data }) {
                 const { slug } = node.fields
                 const { title } = node.frontmatter
                 return (
-                  <PostRep key={slug}>
+                  <PostItem key={slug}>
                     <div className="post-rep">
-
-                      <PostRepHeader>
-                        <Link to={slug}>{title}</Link>
-                      </PostRepHeader>
-                      
-                      <PostRepDate className="post-rep-date">
-                        {node.frontmatter.date}
-                      </PostRepDate>
-
-                      <PostRepTags>
-                        <TagList tags={node.frontmatter.tags} />
-                      </PostRepTags>
-
-                      <PostRepExcerpt className="post-rep-excerpt">
-                        {node.excerpt}
-                      </PostRepExcerpt>
+                    <PostRep
+                      title = {title}
+                      tags = {node.frontmatter.tags}
+                      date = {node.frontmatter.date}
+                      excerpt = {node.excerpt}
+                      slug = {slug}
+                    />
                     </div>
-                  </PostRep>
+                  </PostItem>
                 )
               })}
             </PostList>
@@ -114,7 +69,7 @@ export const pageQuery = graphql`
     ) {
       edges {
         node {
-          excerpt
+          excerpt(format: PLAIN)
           fields {
             slug
           }
