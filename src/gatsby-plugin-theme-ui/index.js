225ccf48448020b16cd221e2bbe9357635134ba5
ijgit <isuu.jeong@gmail.com>
2020-12-19 08:23:47 +0900

    change inline code color

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -5,14 +5,12 @@ export default {
     text: '#24292e',
     primary: '#24292e',
     background: '#fff',
-    code: '#24292e',
     modes: {
       dark: {
         text: '#e4e4e4',
         primary: '#e4e4e4',
         blockquote: '#e4e4e4',
-        background: '#212121',
-        code: '#e4e4e4',
+        background: '#262626',
       },
     },
   },
