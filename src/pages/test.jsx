464a0635a57f461140af9dca50a3d814848fa270
ijgit <isuu.jeong@gmail.com>
2020-12-20 23:35:14 +0900

    add search function in test page

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -7,33 +7,113 @@ import {PostList} from '../components/post-list/index'
 import styled from "styled-components";
 import queryString from 'query-string';
 
-const Item = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  .title{
-    display: flex;
-    align-self: stretch;
-  }
-  .tags{
-    display: flex;
-  }
-  .date{
-    display: flex;
-  }
-`
+import * as JsSearch from "js-search"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+import {Icon} from './../components/icon'
 
+
+const SearchContainer = styled.div`
+display: flex;
+width: 100%;
+max-width: 600px;
+flex-direction: column;
+margin: 0 auto;
+margin-bottom: 10vh;
+
+.search-input{
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  input{
+    flex: auto;
+  }
+}
+.search-count{
+  display: flex;
+  margin-right: auto;
+  opacity: .7;
+}
+`
 
 
 export default function IndexPage({ data }) {
   const { edges } = data.allMarkdownRemark
   const {title} = data.site.siteMetadata;
+  var posts = [];
+  edges.map(({ node }) => {
+    var _node = {
+      id: node.id,
+      slug: node.fields.slug,
+      title: node.frontmatter.title,
+      date: node.frontmatter.date,
+      tags: node.frontmatter.tags,
+      excerpt: node.excerpt,
+    }
+    posts.push(_node)
+  });
+  const [search, setSearch] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const [postList, setPostList] = useState([])
+  const [searchQuery, setSearchQuery] = useState("")
+  const [queryResults, setQueryResults] = useState(posts)
+
+  const rebuildIndex = () => {
+    const dataToSearch = new JsSearch.Search("id")
+    dataToSearch.indexStrategy = new JsSearch.PrefixIndexStrategy()
+    dataToSearch.sanitizer = new JsSearch.LowerCaseSanitizer()
+
+    dataToSearch.searchIndex = new JsSearch.TfIdfSearchIndex("id")
+
+    dataToSearch.addIndex("title")
+    dataToSearch.addIndex("excerpt")
+    dataToSearch.addIndex("tags")
+    dataToSearch.addDocuments(posts)
+
+    setSearch(dataToSearch)
+  }
+
+
+  const searchData = e => {
+    const queryResult = search.search(e.target.value)
+    setSearchQuery(e.target.value)
+    setQueryResults(searchQuery === "" ? postList : searchResults)
+    setSearchResults(queryResult)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  useEffect(() => {
+    setPostList(posts)
+    rebuildIndex()
+  }, [])
+
+
+
+
   return (
     <>
     <Head title={title}/>
     <Layout siteData={data.site}>
         <section id="content">
-        <PostList posts={edges}/>
+        <SearchContainer>
+            <div className='search-input' onSubmit={handleSubmit}>
+              <input
+                id="Search"
+                style={{ padding: "4px 8px" }}
+                value={searchQuery}
+                onChange={searchData}
+                placeholder="Enter your search here"
+              />
+              <label htmlFor="Search" style={{ paddingLeft: "10px" }}>
+                <Icon icon={faSearch}/>
+              </label>
+            </div>
+          </SearchContainer>
+
+
+        <PostList isSearchpage={true} posts={queryResults}/>
         </section>
     </Layout>
     </>
