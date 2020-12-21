5ced3995d64c48ffa5126ce48fdaec4ecde2a2ce
ijgit <isuu.jeong@gmail.com>
2020-12-21 16:35:37 +0900

    change index-page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,115 @@
-import React from "react"
-
+import React, { Component, useEffect, useState, useMemo } from "react"
+import Select from "react-select"
+import kebabCase from "lodash/kebabCase"
 import { Layout } from "../components/layout/layout"
-import { graphql } from "gatsby"
-import {Head} from './../components/head/head'
-import {PostList} from '../components/post-list/post-list'
+import { Link, graphql } from "gatsby"
+import { Head } from "../components/head/head"
+import { PostList } from "../components/post-list/index"
+import styled from "styled-components"
+import queryString from "query-string"
 
+import * as JsSearch from "js-search"
+import { faSearch, faTags } from "@fortawesome/free-solid-svg-icons"
+import { Icon } from "./../components/icon"
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: 0 auto;
+  margin-bottom: 2vh;
+
+  .tag-container {
+    width: 100%;
+    
+    .tag-item{
+      margin: 4px;
+    }
+
+    margin-bottom: 8vh;
+  }
+
+  .search-input {
+    margin-left: auto;
+
+    width: 60%;
+    max-width: 400px;
+
+    display: flex;
+    justify-content: space-between;
+
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    border: 1px solid #8383837e;
+
+    opacity: 0.6;
+    font-size: 0.9em;
+    padding: 5px;
+
+    #search {
+      display: inline-flex;
+      max-width: 90%;
+      border: none;
+      outline: none;
+      background: none;
+      color: inherit;
+    }
+    .search-icon {
+      display: inline-flex;
+    }
+  }
+`
 
 export default function IndexPage({ data }) {
-  const { edges } = data.allMarkdownRemark
-  const {title} = data.site.siteMetadata;
+  const { group, edges } = data.allMarkdownRemark
+  const { title, siteUrl } = data.site.siteMetadata
+  const query = queryString.parse(window.location.search)
+
+  const tag =
+    query.tag === null || query.tag === undefined
+      ? "null"
+      : query.tag.toLowerCase()
+  
+  const posts = (tag == 'null') ? edges : edges.filter(({ node }) => {
+    const tags = node.frontmatter.tags.map(tag => {
+      tag = tag
+        .split(/[\_\ \. \/]/)
+        .join("-")
+        .split(/[\_\ \. \/ \+]/)
+        .join("")
+      return tag.toLowerCase()
+    })
+    if (tags.includes(tag)) {
+      return node
+    }
+  })
+
+
   return (
     <>
-    <Head title={title}/>
-    <Layout siteData={data.site}>
+      <Head title={title} />
+      <Layout siteData={data.site}>
         <section id="content">
-        <PostList posts={edges}/>
+          <div>
+            <Container>
+              <div className="tag-container">
+                {group.map(item => {
+                  return (
+                    <div className="tag-item" key={item.fieldValue}>
+                      <Link to={`?tag=${kebabCase(item.fieldValue)}`}>
+                        {item.fieldValue}
+                      </Link>
+                    </div>
+                  )
+                })}
+              </div>
+            </Container>
+          </div>
+
+          <PostList posts={posts} />
         </section>
-    </Layout>
+      </Layout>
     </>
   )
 }
@@ -26,23 +119,29 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+        siteUrl
       }
+      pathPrefix
     }
     allMarkdownRemark(
-      limit: 5
+      limit: 2000
       sort: { fields: [frontmatter___date], order: DESC }
-      filter: {frontmatter: {draft: {ne: false}}}
+      filter: { frontmatter: { draft: { ne: false } } }
     ) {
+      group(field: frontmatter___tags) {
+        fieldValue
+      }
       edges {
         node {
-          excerpt
+          id
           fields {
             slug
           }
+          excerpt
           frontmatter {
+            date(formatString: "YYYY-MM-DD")
             tags
             title
-            date(formatString: "YYYY-MM-DD")
           }
         }
       }
