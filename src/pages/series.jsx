cc493676d6b8eb7cc54f3f5a1a8773b1cce537eb
ijgit <isuu.jeong@gmail.com>
2021-03-01 04:58:22 +0900

    [feat] serise page - add main image

--- src/pages/series.jsx
+++ src/pages/series.jsx
@@ -3,29 +3,29 @@ import { Layout } from "../components/layout/layout"
 import { Link, graphql } from "gatsby"
 import { Head } from "../components/head/head"
 import styled from "styled-components"
+import Img from "gatsby-image"
+import imageInfo from "./../../config/series-config"
 
 const GalleryContainer = styled.div`
   .gallery {
     display: flex;
 
-
-    .figure-item{
+    .figure-item {
       margin: 8px;
       max-width: 300px;
       width: 300px;
-
-      @media screen and (max-width: 479px){
+      @media screen and (max-width: 479px) {
         width: 95%;
       }
-
       .figure {
         width: 100%;
+  
         border-radius: 10px 10px 0 0;
         object-fit: cover;
-      }  
-      
+      }
+
       .figure-caption {
-        display: inline-block;  
+        display: inline-block;
         padding-left: 8px;
 
         padding: 8px;
@@ -33,7 +33,7 @@ const GalleryContainer = styled.div`
 
         background-color: #43494f86;
         border-radius: 0 0 10px 10px;
-        
+
         font-size: calc(13px + 0.3vh);
         font-weight: 500;
 
@@ -41,63 +41,18 @@ const GalleryContainer = styled.div`
         overflow: hidden;
         text-overflow: ellipsis;
       }
-      
-    }
-
-  }
-`
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  margin: 0 auto;
-  margin-bottom: 2vh;
-
-  .tag-container {
-    width: 100%;
-
-    .tag-item {
-      margin: 4px;
-    }
-
-    margin-bottom: 5vh;
-  }
-
-  .search-input {
-    margin-left: auto;
-
-    width: 60%;
-    max-width: 400px;
-
-    display: flex;
-    justify-content: space-between;
-
-    -webkit-border-radius: 5px;
-    -moz-border-radius: 5px;
-    border-radius: 5px;
-    border: 1px solid #8383837e;
-
-    opacity: 0.6;
-    font-size: 0.9em;
-    padding: 5px;
-
-    #search {
-      display: inline-flex;
-      max-width: 90%;
-      border: none;
-      outline: none;
-      background: none;
-      color: inherit;
-    }
-    .search-icon {
-      display: inline-flex;
     }
   }
 `
 
 export default function IndexPage({ data }) {
   const { group } = data.allMarkdownRemark
+  const { edges } = data.allFile // images
+
+  var main_images = {}
+  edges.forEach(({ node }) => {
+    main_images[node.name] = node.childImageSharp.fixed
+  })
 
   return (
     <>
@@ -107,18 +62,15 @@ export default function IndexPage({ data }) {
           <div className="gallery">
             {group.map(item => {
               return (
-                <Link
-                  to={`/series/${item.fieldValue}`}
-                  className="figure-item"
-                  key={item.fieldValue}
-                >
-                  <img
-                    className="figure"
-                    src="https://miro.medium.com/max/453/1*51D0MqtqHu3h2vTE5oJ-7g.png"
-
-                  />
-                  <div className="figure-caption">{item.fieldValue}</div>
-                </Link>
+                <div key={item.fieldValue} className="figure-item">
+                  <Link to={`/series/${item.fieldValue}`}>
+                    <Img
+                      className='figure'
+                      fixed={main_images[imageInfo[item.fieldValue]]}
+                    />
+                    <div className="figure-caption">{item.fieldValue}</div>
+                  </Link>
+                </div>
               )
             })}
           </div>
@@ -147,6 +99,25 @@ export const pageQuery = graphql`
           fields {
             slug
           }
+        }
+      }
+    }
+
+    allFile(
+      filter: {
+        sourceInstanceName: { eq: "images" }
+        relativeDirectory: { eq: "main-images" }
+      }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            fixed(height: 200, width: 300) {
+              src
+              ...GatsbyImageSharpFixed
+            }
+          }
+          name
         }
       }
     }
