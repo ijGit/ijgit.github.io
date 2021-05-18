6cf3b8d03f49393fa580b217a9272098995bfa2b
ijgit <isuu.jeong@gmail.com>
2021-05-18 23:20:40 +0900

    Create Empty Branch
    If applied, this commit will remove all files (initialize)

--- gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-// prismJS
-//require("prismjs/themes/prism-solarizedlight.css");
-require("prismjs/plugins/line-numbers/prism-line-numbers.css");
-require("prismjs/plugins/command-line/prism-command-line.css");
--- src/utils/typography.js
+++
@@ -1,9 +0,0 @@
-import Typography from 'typography';
-const typography = new Typography({
-  baseFontSize: '16px',
-  baseLineHeight: 1.6,
-})
-export default typography;
-
-
-
