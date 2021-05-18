5ca93c5e4ac60340f73000dc712cbfb585918d01
ijgit <isuu.jeong@gmail.com>
2021-05-19 05:10:55 +0900

    add files

---
+++ config/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+  /* blog setting */
+
+  // metadata (important)
+  title: `ij.Log`,
+  description: `isu jeong, github page blog`,
+  author: `isujeong`,
+  language: `ko-KR`,
+  prefix: '/',
+  url: `https://ijgit.github.io`,
+
+  // Bio 
+  comment: `hello world`,
+  name: `isu.jeong`,
+  email: `isuu.jeong@gmail.com`,
+  github: 'https://github.com/ijgit',
+  
+  // option 
+  googleAnalytics: ``,
+  googleSearchConsole: `5k_bJRZxnC2wJI8Pk2JL9KiZN-2dtWifGrMUhkH6QAg`, 
+  googleAdsenseSlot: ``,  
+  googleAdsenseClient: ``, 
+
+  /* comment */
+  githubInfo: `ijgit/ijgit.github.io`, // username/username.github.io 
+
+}
---
+++ config/type.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'default': '📄',
+  'main': '',
+  'snippet': '🧩',
+  'design': '🎨',
+  'tool': '🛠️',
+  'theory': '📚',
+  'study': '📚',
+  'term': '📒',
+  'develop': '💻',
+  'problem': '🔑',
+  'review': '📰',
+  'ref': '💬',
+  'memo': '📌',
+  'note': '✏️',
+  'error': '🚨',
+}
+
+// 📅 💬 🗝 🔑 🗒 🗃 ⚠️ 💻 💾 🤖 🛠 🚩 🔧🔬📰😎👨🏻‍💻
+//'note': '🖋️',
