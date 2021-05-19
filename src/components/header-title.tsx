72e9926846ec5c4d83e41b4972b27f4ab72cd983
ijgit <isuu.jeong@gmail.com>
2021-05-19 13:28:52 +0900

    Create header

---
+++ src/components/header-title.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const HeaderTitle = ( ) => {
+  return(
+    <div
+      style={{
+        display: `flex`,
+      }}
+    >
+      
+    </div>
+  )
+};
---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Header = ( ) => {
+  return(
+    <header
+      style={{
+        height: `5vh`,
+        width: `100%`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        alignItems: `stretch`
+      }}
+    >
+    </header>
+  )
+};
