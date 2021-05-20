175e3de2285a542e313360e7b24beef4e4a87461
ijgit <isuu.jeong@gmail.com>
2021-05-20 21:38:28 +0900

    Update tag-list.tsx
    set tag color (fixed)
    random 색상 이상..

--- src/components/tag-list.tsx
+++ src/components/tag-list.tsx
@@ -11,37 +11,9 @@ export const TagList = ({ tags }) => {
       }}
     >
       {tags.map((tag) => {
-        
-        var tagColor = [0, 0, 0];
-        var tagColorIdx = 0;
-        for (var i=0; i<tag.length; i++){
-          tagColor[tagColorIdx] += tag.charCodeAt(i);
-          tagColor[tagColorIdx] %= 256;
-          tagColorIdx = (tagColorIdx+1)%3; 
-        }
-
-        for (var i=0; i<tag.length; i++){
-          if(tagColor[i] < 100){
-            tagColor[i] += 120;
-            break;
-          } 
-        }
-
-        var newColor = `rgb(${tagColor[0]}, ${tagColor[1]}, ${tagColor[2]})`;
-        console.log(newColor);
-        
-                
-
         return (
           <>
-
             <Badge variant="tag"
-              style={{
-                background: newColor,
-                color: '#000000'
-              }} 
-              
-            
               key={tag}>
               <Link to={`/?tag=${tag.split(" ").join("")}`}>{tag}</Link>
             </Badge>
